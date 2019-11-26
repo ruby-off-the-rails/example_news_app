0be77600b48655e8db5609604379826e5126c349
brian <riceb53@gmail.com>
2019-11-25 21:14:59 -0600

    add http request to news api

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,5 +1,6 @@
 class Api::ArticlesController < ApplicationController
   def index
+    @response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search_term]}&from=2019-10-26&sortBy=publishedAt&apiKey=").parse
     render 'index.json.jb'
   end
 end
